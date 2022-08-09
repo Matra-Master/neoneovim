6ee65e08f145bb07d7bd9c117ed9cd2fdeeb647c
Franco Carabajal <carabajal.francogabriel@gmail.com>
2022-08-08 21:07:36 -0300

    Added telescope to packer
    It is not configured yet

--- lua/demaster/packer-plugins.lua
+++ lua/demaster/packer-plugins.lua
@@ -14,6 +14,11 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'folke/tokyonight.nvim'
 
+  -- Essentials and utils
+  use("nvim-lua/plenary.nvim")
+  use("nvim-lua/popup.nvim")
+  use("nvim-telescope/telescope.nvim")
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then

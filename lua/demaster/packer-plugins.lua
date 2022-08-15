2a09d41af102b5401846ea6c8dab508308e94501
Franco Carabajal <carabajal.francogabriel@gmail.com>
2022-08-15 00:06:48 -0300

    Treesitter plugin added

--- lua/demaster/packer-plugins.lua
+++ lua/demaster/packer-plugins.lua
@@ -20,6 +20,9 @@ return require('packer').startup(function(use)
   use("nvim-telescope/telescope.nvim")
   use("tpope/vim-fugitive")
 
+  use { 'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then

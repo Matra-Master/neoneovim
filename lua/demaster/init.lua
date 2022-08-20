79d639a685083fc639ef4b2236b4f350f065039b
Franco Carabajal <carabajal.francogabriel@gmail.com>
2022-08-20 20:34:46 -0300

    Made lspconfig for python TDD style
    Messy and unusable, but functional. Now i have to add mason to the
    equation and use it to install pyright

--- lua/demaster/init.lua
+++ lua/demaster/init.lua
@@ -2,3 +2,24 @@ require("demaster.packer-plugins")
 require("demaster.sets")
 require("demaster.remaps")
 print("Hello Demaster")
+
+--Made it work TDD style
+--just made it work, for python server and without any new separated shinny config file
+--Now i should do the refactor style shit
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', '<leader>bf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
+require"lspconfig".pyright.setup{
+  on_attach = on_attach,
+  flags = {
+    debounce_text_changes = 150,
+  }
+}
--- lua/demaster/packer-plugins.lua
+++ lua/demaster/packer-plugins.lua
@@ -24,6 +24,9 @@ return require('packer').startup(function(use)
   use { 'nvim-treesitter/nvim-treesitter',
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
   }
+
+  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then

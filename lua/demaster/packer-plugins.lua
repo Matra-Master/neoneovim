6da4fae71fdbf237e10650c9f4e18835c9866b74
Franco Carabajal <carabajal.francogabriel@gmail.com>
2023-04-10 10:02:45 -0300

    Don't know why the uppercase variable but seems an innocent change

--- lua/demaster/packer-plugins.lua
+++ lua/demaster/packer-plugins.lua
@@ -4,7 +4,7 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  Packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 -- Only required if you have packer configured as `opt`
   vim.cmd [[packadd packer.nvim]]
 end
@@ -54,7 +54,7 @@ return require('packer').startup(function(use)
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
-  if packer_bootstrap then
+  if Packer_bootstrap then
     require('packer').sync()
   end
 

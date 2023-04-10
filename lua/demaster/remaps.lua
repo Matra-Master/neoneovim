00fe7ca687290c5dd70d8c67fb4146497471e555
Franco Carabajal <carabajal.francogabriel@gmail.com>
2023-04-10 09:55:38 -0300

    Removed theming for Telescope live_grep keymap

--- lua/demaster/remaps.lua
+++ lua/demaster/remaps.lua
@@ -127,7 +127,7 @@ nnoremap ('<leader>jo', ':lua require("harpoon.term").sendCommand(1, 2)<CR>', {d
 --     Telescope
 --
 nnoremap ("<leader>f", ":Telescope find_files<CR>", { desc = 'Search [F]iles' })
-nnoremap ("<leader>ri", ":Telescope live_grep theme=get_dropdown<CR>", { desc = 'Search by Grep' })
+nnoremap ("<leader>ri", ":Telescope live_grep<CR>", { desc = 'Search by Grep' })
 nnoremap ("<leader>t", ":Telescope <CR>", { desc = '[T]elescope Built-ins'})
 nnoremap ("<leader>tg", ":Telescope git_files<CR>", { desc = '[T]elescope [G]it builtin'})
 nnoremap ("<leader>jJ", ":Telescope harpoon marks<CR>", { desc = 'Telescope with Harpoon!'})

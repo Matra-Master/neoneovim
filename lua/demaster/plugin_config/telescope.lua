c5669f99977ed36eb88863e4393ec26dbd9f852e
Franco Carabajal <carabajal.francogabriel@gmail.com>
2023-05-17 17:30:38 -0300

    Added a setup for telescope so i can delete buffers from the picker

--- lua/demaster/plugin_config/telescope.lua
+++ lua/demaster/plugin_config/telescope.lua
@@ -1,1 +1,17 @@
+require("telescope").setup {
+  pickers = {
+    buffers = {
+      mappings = {
+        n = {
+          ['<c-d>'] = require('telescope.actions').delete_buffer
+        }, -- n
+        i = {
+          ["<C-h>"] = "which_key",
+          ['<c-d>'] = require('telescope.actions').delete_buffer
+        } -- i
+      } -- mappings
+    }
+  }
+}
+
 require("telescope").load_extension('harpoon')

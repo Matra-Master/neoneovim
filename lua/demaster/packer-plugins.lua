f2ca25435c9fc6dd5c30ef32d5cda150a4c76d59
Franco Carabajal <carabajal.francogabriel@gmail.com>
2024-03-25 16:25:07 -0300

    style(colorscheme): installed catppuccin theme

--- lua/demaster/packer-plugins.lua
+++ lua/demaster/packer-plugins.lua
@@ -22,9 +22,11 @@ return require('packer').startup(function(use)
 
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
   -- Themes
   use 'folke/tokyonight.nvim'
   use 'ellisonleao/gruvbox.nvim'
+  use { "catppuccin/nvim", as = "catppuccin" }
 
   use { 'nvim-treesitter/nvim-treesitter',
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
